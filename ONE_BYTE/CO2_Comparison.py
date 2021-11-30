91a5260b6ec6e1c6713ccf4fa73dd4ed787140b9
mjacoupy <maxime.jacoupy@gmail.com>
2021-11-30 15:22:53 +0100

    cleaning

--- ONE_BYTE/CO2_Comparison.py
+++ ONE_BYTE/CO2_Comparison.py
@@ -239,7 +239,7 @@ elif analysis == "Bilan Carbone - Comparatifs":
     device_ordi = CONSO_LAPTOP * MINUTES_PAR_JOUR * JOURS_PAR_SEMAINE * SEMAINE_PAR_AN
     device_tel = CONSO_SMARTPHONE * MINUTES_PAR_JOUR * JOURS_PAR_SEMAINE * SEMAINE_PAR_AN
 
-    col0, col1, col2, col3, col4, col5 = st.columns(6)
+    col0, col1, col2, col3, col4, col5 = st.columns([1, 3, 3, 3, 3, 3])
     with col1:
         st.markdown("[1] **LAPTOP SEUL**")
     with col2:
